b32aff0f6eafda76263402bff68ab3b403dac6aa
FAHAD <faddukhan3@gmail.com>
2026-01-30 13:08:27 +0000

    test

--- Phase-1/test/02.js
+++ Phase-1/test/02.js
@@ -1,8 +1,8 @@
 import readline from 'readline-sync'
 
 let year = readline.questionInt('enter your year: ')
-if ((year + 4) % 2 == 0){
+if (year % 2 == 0) {
     console.log('it is a leap year');
-}else{
+} else {
     console.log('it is not a leap year');
 }
