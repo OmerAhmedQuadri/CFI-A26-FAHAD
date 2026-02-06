a3a0aa705acc2c2953170184ca0ca70c16e94a5d
FAHAD <faddukhan3@gmail.com>
2026-02-06 11:53:27 +0000

    06 - feb

---
+++ Phase-1/06-dates-time/01.js
@@ -0,0 +1,14 @@
+const now = new Date()
+console.log(now);
+console.log(Date.now());
+console.log(now.toDateString());
+console.log(now.toISOString());
+console.log(now.toLocaleString());
+console.log(now.toLocaleDateString());
+console.log(now.toUTCString());
+console.log(now.toString());
+console.log(now.getHours());
+console.log(now.setDate(11));
+// let a = now.constructor
+// console.log(a());
+ console.log(new Date());
--- Phase-1/07-algos/01.js
+++ Phase-1/07-algos/01.js
@@ -1,5 +1,5 @@
 //write a fucntion 2 inputs arr and target and rreturns the index of the target element if found in the array
-
+import readline from 'readline-sync'
 function linearsearch(arr, target) {
     if (!(Array.isArray(arr))) {
         return 'enter a valid array'
@@ -12,4 +12,33 @@ function linearsearch(arr, target) {
     } return -1
 }
 
-console.log(linearsearch([1, 2, 33, 4, 5], 3));
+
+(function wrap() {
+    while (true) {
+        console.log("1.linear search\n2.exit");
+        let choice = readline.questionInt("enter your choice: ")
+        switch (choice) {
+            case 1:
+                let size = readline.questionInt("enter the size of the array: ")
+                let num = []
+                for (let i = 0; i < size; i++) {
+                    num.push(readline.questionInt(`enter the ${i} element: `))
+                }
+                let target = readline.questionInt("enter the target element: ")
+                let op = linearsearch(num, target)
+                if (op == -1) {
+                    console.log('element not found');
+                }
+                else if (op == undefined) {
+                    console.log('enter a valid array');
+                }
+                else {
+                    console.log('element found at', op);
+                }
+                break;
+            default:
+                return
+        }
+    }
+})()
+
