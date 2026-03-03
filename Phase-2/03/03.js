d8f9631855954c0f3d8e618a003dde57c3e4edf3
FAHAD <faddukhan3@gmail.com>
2026-03-03 19:44:10 +0000

    02-3

--- Phase-2/03/03.js
+++ Phase-2/03/03.js
@@ -6,16 +6,16 @@ dotenv.config()
 const pass = process.env.password
 console.log(pass);
 
-// const hashPassword = async (password) => {
-//     const hashedPassword = await bcrypt.hash(password, 12)
-//     return hashedPassword
-// }
+const hashPassword = async (password) => {
+    const hashedPassword = await bcrypt.hash(password, 12)
+    return hashedPassword
+}
 
-// console.log(await hashPassword(pass));
+console.log(await hashPassword(pass));
 
 const comparePassword = async (password, hashedPassword) => {
     const match = await bcrypt.compare(password, hashedPassword)
     return match
 }
 
-console.log(await comparePassword(pass, '$2b$12$XpfUvJFNL5CxSILvwuOsb.rr.7HhBruSA1gIdTKIk4mLqaw61Z5ra'));
+// console.log(await comparePassword(pass, '$2b$12$XpfUvJFNL5CxSILvwuOsb.rr.7HhBruSA1gIdTKIk4mLqaw61Z5ra'));
